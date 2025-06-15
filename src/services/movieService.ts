d295ad2b122e2b9fd25cc6ebc650b7def84f507e
tanya-yanko <yanko200662@gmail.com>
2025-06-15 10:25:56 +0300

    fixed3

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,27 +1,21 @@
 import { type Movie } from "../types/movie.ts";
+
 import axios from "axios";
 
 export interface GetMovieRes {
   results: Movie[];
 }
-
-const BASE_URL = "https://api.themoviedb.org/3";
-const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
-
+export const axiosConfig = {
+  url: "https://api.themoviedb.org/3/search/movie",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+  },
+};
 export const fetchMovies = async (newQuery: string): Promise<Movie[]> => {
-  if (!TOKEN) {
-    throw new Error("TMDB token is missing in .env");
-  }
-
-  const res = await axios.get<GetMovieRes>(`${BASE_URL}/search/movie`, {
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${TOKEN}`,
-    },
-    params: {
-      query: newQuery,
-    },
-  });
-
+  const res = await axios.get<GetMovieRes>(
+    `${axiosConfig.url}?query=${newQuery}`,
+    axiosConfig
+  );
   return res.data.results;
 };
